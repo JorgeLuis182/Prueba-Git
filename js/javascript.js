ca330f34321b789be9147e5f1908b7f40d7c1168
unknown <JorgeLuis182>
2022-11-15 19:53:46 -0500

    actualizacion 15 de Nov 2022

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,14 @@
  * and open the template in the editor.
  */
 
-function saludo(){
-    Swal.fire("Hola","Mundo","success");
+function saludo() {
+    // Swal.fire("Hola","Mundo","success");
+    Swal.fire({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://cdn.pixabay.com/photo/2016/02/10/21/59/landscape-1192669__340.jpg',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+    });
 }
